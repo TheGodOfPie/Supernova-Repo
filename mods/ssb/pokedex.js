5c55b35c967ac582743a9a1c72a91886501e425d
TheGodOfPie <quijanoandrei8@gmail.com>
2016-05-02 15:17:47 +0000

    pokedex.js created online with Bitbucket

---
+++ mods/ssb/pokedex.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.BattlePokedex = {
+	//@Supernova Robot
+	palkia: {
+		inherit: true,
+		basespecies: "Nidoran-M",
+		forme: "Mega",
+		formeLetter: "M",
+		abilities:{0:"Galactic Clash"},
+	},
+	nidoranm: {
+		inherit: true,
+		otherForms: ["Palkia"],
+	},
+};
