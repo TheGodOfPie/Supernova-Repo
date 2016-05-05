bb7f4042c3c4cc25630b224a4adce3b1439a29fa
Sundar <steelchar90@gmail.com>
2016-05-05 05:53:28 +0000

    Add in Trainer Cards

--- chat-plugins/trainercards.js
+++ chat-plugins/trainercards.js
@@ -1,9 +1,18 @@
+/**************************
+* Trainer Cards Plugin    * 
+*                         *
+* Made by: Steel Sciz     *
+*  						  *
+* PS Plugin to add in TCs *
+*   Utilizing OriginDB     *
+*                         *
+**************************/
 'use strict';
 
 let fs = require('fs');
 let path = require('path');
 
-function logTcs(message) {
+function logTc(message) {
 	if (!message) return;
 	let file = path.join(__dirname, '../logs/tc.txt');
 	let date = "[" + new Date().toUTCString() + "] ";
@@ -23,19 +32,38 @@ exports.commands = {
 		Db('tc').set(name, tc);
 		
 		this.sendReply("Trainer Card has been set.");
-		if (Users.get(username)) Users(username).popup(user.name + " has given you the title: ||||" + title);
-		logTitle(" + user.name + " has added the following Trainer Card: ' + name + ' .");
+		logTc(user.name + " has added the following Trainer Card: " + name + ".");
 	},
+
 	addtchelp: ["/addtc [name], [HTML] - Adds a trainer card."],
-    
-    },
     
     tc: function (target, room, user) {
       if (!this.runBroadcast()) return; 
-      if (!target || target.indexOf(',') < 0) return this.parse('/help tc');     
-	  let name = toId(parts[0]);      
-      Db('tc').get(name);     
-      this.parse("|html| ' + name + '");
+      if (!target < 0) return this.parse('/help tc');     
+	  let name = Db('tc').get(target);         
+      this.sendReplyBox('' + name + '');
     },
-      
+
+	tchelp: ["/tc [name] - Broadcasts a Trainer Card."],     
+
+	deletetc: function (target, room, user) {
+	  if (!this.can('hotpatch')) return false;
+      if (!target < 0) return this.parse('/help deletetc');
+      let name = Db('tc').get(target);
+
+      Db('tc').delete(target);
+
+      this.sendReply('You have successfully deleted the Trainer Card ' + target + '.');
+
+    },
+    deletetchelp: ["/deletetc [name] - Delete a trainer card from the list of tcs that you have."],
+
+/***************************************************
+* Copied from Dragotic's VIP and Developer Commands *
+****************************************************/
+	tclist: function (target, room, user) {
+		if (!this.can('hotpatch')) return false;
+		if (!Object.keys(Db('tc').object()).length) return this.errorReply('There seems to be no trainer cards present at the moment.');
+		this.sendReplyBox('<center><b><u>Trainer Card List:</u></b></center>' + '<br /><br />' + Object.keys(Db('tc').object()).join('<br />'));
+
 };
