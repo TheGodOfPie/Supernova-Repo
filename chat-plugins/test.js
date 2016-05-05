bd622103acdce1a8d03873750068506f66c235a2
Sundar_s <steelchar90@gmail.com>
2016-05-05 05:57:21 +0000

    Keep it as a test

---
+++ chat-plugins/test.js
@@ -0,0 +1,60 @@
+/**************************
+* Trainer Cards Plugin    * 
+*                         *
+* Made by: Steel Sciz     *
+*  						  *
+* PS Plugin to add in TCs *
+*   Ultizing OriginDB     *
+*                         *
+**************************/
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+
+function logTc(message) {
+	if (!message) return;
+	let file = path.join(__dirname, '../logs/tc.txt');
+	let date = "[" + new Date().toUTCString() + "] ";
+	let msg = message + "\n";
+	fs.appendFile(file, date + msg);
+}
+
+exports.commands = {
+	addtc: function (target, room, user) {
+		if (!this.can('forcewin')) return false;
+		if (!target || target.indexOf(',') < 0) return this.parse('/help addtc');
+
+		let parts = target.split(',');
+		let name = toId(parts[0]);
+		let tc = parts.slice(1).join(", ").trim();
+
+		Db('tc').set(name, tc);
+		
+		this.sendReply("Trainer Card has been set.");
+		logTc(user.name + " has added the following Trainer Card: " + name + ".");
+	},
+
+	addtchelp: ["/addtc [name], [HTML] - Adds a trainer card."],
+    
+    tc: function (target, room, user) {
+      if (!this.runBroadcast()) return; 
+      if (!target < 0) return this.parse('/help tc');     
+	  let name = Db('tc').get(target);         
+      this.sendReplyBox('' + name + '');
+    },
+
+	tchelp: ["/tc [name] - Broadcasts a Trainer Card."],     
+
+	deletetc: function (target, room, user) {
+	  if (!this.can('hotpatch')) return false;
+      if (!target < 0) return this.parse('/help deletetc');
+      let name = Db('tc').get(target);
+
+      Db('tc').delete(target);
+
+      this.sendReply('You have successfully deleted the Trainer Card ' + target + '.');
+
+    },
+    deletetchelp: ["/deletetc [name] - Delete a trainer card from the list of tcs that you have."]
+};
