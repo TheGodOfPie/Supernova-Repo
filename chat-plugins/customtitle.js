fc23c559c798fcb19754df1c5af61d41d930cd35
Dragotic <pokemon.ahmed@gmail.com>
2016-05-12 17:04:20 +0000

    customtitle.js edited online with Bitbucket

--- chat-plugins/customtitle.js
+++ chat-plugins/customtitle.js
@@ -6,16 +6,10 @@
 
 'use strict';
 
-global.hasTitle = function (user) {
-	if (!user) return;
-	let targetUser = Db('titles').get(toId(user));
-	if (targetUser === 1) return true;
-	return false;
-};
-
 exports.commands = {
 	settitle: 'customtitle',
 	customtitle: {
+		set: 'give',
 		give: function (target, room, user) {
 			if (!this.can('eval')) return false;
 			if (!target) return this.parse('/help customtitle');
@@ -29,19 +23,19 @@ exports.commands = {
             .set([targetUser, 'Color'], color);
 			this.sendReply(targetUser + ' has received a custom title.');
 		},
-        /*
+		remove: 'take',
 		take: function (target, room, user) {
 			if (!this.can('eval')) return false;
-			let devUser = toId(target);
-			if (!this) return this.parse('/help dev');
-			if (!isDev(this)) return this.errorReply(this + ' is not a developer.');
-			Db('devs').delete(this);
-			this.sendReply(this + '\'s developer status has been taken.');
+			let targetUser = toId(target);
+			if (!targetUser) return this.parse('/help customtitle');
+			if (!Db('titles').has(targetUser)) return this.errorReply(targetUser + 'does not have a custom title.');
+			Db('titles').delete(targetUser);
+			this.sendReply(targetUser + '\'s custom title has been removed.');
 		},
 		list: function (target, room, user) {
 			if (!this.can('eval')) return false;
-			if (!Object.keys(Db('devs').object()).length) return this.errorReply('There seems to be no user with dev status.');
-			this.sendReplyBox('<center><b><u>Developers</u></b></center>' + '<br /><br />' + Object.keys(Db('devs').object()).join('<br />'));
+			if (!Object.keys(Db('titles').object()).length) return this.errorReply('There seems to be no user with custom title.');
+			this.sendReplyBox('<center><b><u>Custom Titles</u></b></center>' + '<br /><br />' + Object.keys(Db('titles').object()).join('<br />'));
 		},
         */
 	},
